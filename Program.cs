bd927556761daad33c02d3fe2423d4720c7e1940
Juan Carlos <jcmgx2006@gmail.com>
2026-04-30 20:36:35 -0500

    avance)

--- Program.cs
+++ Program.cs
@@ -7,11 +7,26 @@ class Program
         string opcion = "";
         double saldo = 1000;
 
-        Console.WriteLine("--- CAJERO AUTOMATICO ---");
-        Console.WriteLine("1. Ver saldo");
-        Console.WriteLine("2. Depositar");
-        Console.WriteLine("3. Retirar");
-        Console.WriteLine("4. Salir");
+        while (opcion != "4")
+        {
+            Console.WriteLine("---- CAJERO AUTOMATICO ----");
+            Console.WriteLine("1. Ver saldo");
+            Console.WriteLine("2. Depositar");
+            Console.WriteLine("3. Retirar");
+            Console.WriteLine("4. Salir");
 
-        Console.Write("Seleccione una opcion: ");
-        opcion = Console.ReadLine() ?? "";
+            Console.Write("Seleccione una opcion: ");
+            opcion = Console.ReadLine() ?? "";
+        }
+    }
+}
+        if (opcion == "1")
+    {
+    Console.WriteLine("Tu saldo es: " + saldo);
+      }
+else if (opcion == "2")
+     {
+    Console.Write("Monto a depositar: ");
+    double dep = double.Parse(Console.ReadLine() ?? "0");
+    saldo += dep;
+       }
