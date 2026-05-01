8ce92837ff314dcde44873badd141833f31059a7
Juan Carlos <jcmgx2006@gmail.com>
2026-04-30 20:53:16 -0500

    commit final)

--- Program.cs
+++ Program.cs
@@ -21,12 +21,41 @@ class Program
     }
 }
         if (opcion == "1")
-    {
+      {
     Console.WriteLine("Tu saldo es: " + saldo);
       }
 else if (opcion == "2")
-     {
+      {
     Console.Write("Monto a depositar: ");
     double dep = double.Parse(Console.ReadLine() ?? "0");
     saldo += dep;
-       }
+        }Console.WriteLine("Deposito exitoso.");
+            }
+            else if (opcion == "3")
+            {
+                Console.Write("Monto a retirar: ");
+                double ret = double.Parse(Console.ReadLine() ?? "0");
+                if (ret <= saldo)
+                {
+                    saldo -= ret;
+                    Console.WriteLine("Retiro exitoso.");
+                }
+                else
+                {
+                    Console.WriteLine("Saldo insuficiente.");
+                }
+            }
+            else if (opcion == "4")
+            {
+                Console.WriteLine("Gracias por usar el cajero.");
+            }
+            else
+            {
+                Console.WriteLine("Opcion invalida.");
+            }
+
+            Console.WriteLine("Presiona una tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
